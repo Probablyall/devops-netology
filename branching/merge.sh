5cbf63cbee1e04eefcac627924f21bef174d15e0
Dmitriy <whitedemon@bk.ru>
2020-10-19 19:27:05 +0300

    merge2

--- branching/merge.sh
+++ branching/merge.sh
@@ -2,8 +2,14 @@
 # display command line options
 
 count=1
+<<<<<<< HEAD
 while [[ -n "$1" ]]; do
     echo "Parameter #$count = $1"
     count=$(( $count + 1 ))
     shift
+=======
+for param in "$*"; do
+    echo "\$* Parameter #$count = $param"
+    count=$(( $count + 1 ))
+>>>>>>> 7bb1f82... 5
 done
