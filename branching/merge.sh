c0ab4e7d97df021e4198317b2bd3b3e5257c9a14
Dmitriy <whitedemon@bk.ru>
2020-10-20 23:15:40 +0300

    merge20102020_2

--- branching/merge.sh
+++ branching/merge.sh
@@ -3,6 +3,7 @@
 
 count=1
 <<<<<<< HEAD
+<<<<<<< HEAD
 while [[ -n "$1" ]]; do
     echo "Parameter #$count = $1"
     count=$(( $count + 1 ))
@@ -12,4 +13,9 @@ for param in "$*"; do
     echo "\$* Parameter #$count = $param"
     count=$(( $count + 1 ))
 >>>>>>> 7bb1f82... 5
+=======
+for param in "$*"; do
+    echo "\$* Parameter #$count = $param"
+    count=$(( $count + 1 ))
+>>>>>>> c41f162... files
 done
